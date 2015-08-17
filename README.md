b23cdfdb9e4ae630f3566dd57c7e03c8f92ac62f
Dominik Babić <domynick93@hotmail.com>
2015-08-17 13:35:37 +0200

    Bootstrap version
    Updated version of the original site using Bootstrap

--- README.md
+++
@@ -1,4 +0,0 @@
-## DomBabic.github.io
-Personal page
-
-Given how the project in current state is not made to be responsive, updating it to a responsive state is the next thing to do.
--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-function update() {
-	document.getElementById("img_container").style.top = "15%";
-	document.getElementById("img_description").style.top = "45%";
-	var toremove = document.getElementById("img_button");
+function update()
+{
+    document.getElementById("content_holder").style.WebkitAnimation = "update 1s forwards";
+    var toremove = document.getElementById("img_button");
 	toremove.parentNode.removeChild(toremove);
-	var new_description = document.createElement("div");
-	new_description.style.backgroundColor = "aliceblue";
-	new_description.style.color = "Black";
-	new_description.style.position = "absolute";
-	new_description.style.top = "60%";
-	new_description.style.left = "35%";
-	new_description.style.width = "30%";
-	new_description.style.height = "20%";
-	new_description.setAttribute("id","new_description");
-	document.getElementById("id_container").appendChild(new_description);
-	var paragraph = document.createElement("p");
+    var new_description = document.createElement("div");
+    new_description.style.opacity = 0;
+    new_description.setAttribute("id","new_description");
+    document.getElementById("content_holder").appendChild(new_description);
+    var paragraph = document.createElement("p");
 	document.getElementById("new_description").appendChild(paragraph);
 	document.getElementById("new_description").firstChild.setAttribute("id", "paragraph_description");
 	document.getElementById("paragraph_description").textContent = description;
+    document.getElementById("new_description").style.WebkitAnimation = "update_text 1s forwards";
 }
 
 var description = "I am a student at the Faculty of Electrical Engineering in Osijek, Croatia currently working on my masters degree in computer engineering. My interests include front-end development and data visualization using D3.js - Data Driven Documents, JavaScript library.";
